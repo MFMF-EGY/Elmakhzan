b11481937c25f119c374029831a81f32a9248914
MFMF-EGY <mohfar55555@gmail.com>
2025-02-05 06:17:31 +0200

    None

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import { useState, useEffect, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-
-
+var StoreID = 1;
+var Response;
 function App() {
+  const [ StoreIDGet, setStoreID ] = useState(1);
+  useEffect(() => {
+    StoreID = StoreIDGet;
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -78,6 +84,7 @@ function SellingTabContent(){
         <button className="Sidebar-button">بحث</button>
         <button className="Sidebar-button">طباعة فاتورة</button>
       </div>
+
     </div>
   );
 }
@@ -92,48 +99,174 @@ function DeleteInvoice(event){
 }
 
 function ProductsTabContent(){
+  const [ ProductID, setProductID ] = useState("");
+  const [ ProductName, setProductName ] = useState("");
+  const [ Trademark, setTrademark ] = useState("");
+  const [ ManufactureCountry, setManufactureCountry ] = useState("");
+  const [ PurchasePrice, setPurchasePrice ] = useState("");
+  const [ WholesalePrice, setWholesalePrice ] = useState("");
+  const [ RetailPrice, setRetailPrice ] = useState("");
+  const [ ProductQuantity, setProductQuantity ] = useState("");
+  const [ AddedProduct, setAddedProduct ] = useState(false);
+  const [Response, setResponse] = useState([]);
+
+  useEffect(() => {
+    var Request = {RequestType:"SearchProducts", StoreID:StoreID};
+    if (ProductID){ Request.ProductID = ProductID; }
+    if (ProductName){ Request.ProductName = ProductName; }
+    if (Trademark){ Request.Trademark = Trademark; }
+    if (ManufactureCountry){ Request.ManufactureCountry = ManufactureCountry; }
+    if (PurchasePrice){ Request.PurchasePrice = PurchasePrice; }
+    if (WholesalePrice){ Request.WholesalePrice = WholesalePrice; }
+    if (RetailPrice){ Request.RetailPrice = RetailPrice; }
+    if (ProductQuantity){ Request.ProductQuantity = ProductQuantity; }
+    axios.get('http://localhost:8000/apis/v1.0/commercial', {params: Request})
+      .then((response)=>{setResponse(response.data)})
+      .catch((err)=>{console.log(err)});
+    setAddedProduct(false);
+  },[]);
+  // useEffect(() => {
+  //   Request = {RequestType:"SearchProducts", StoreID:StoreID};
+  //   if (ProductID){ Request.ProductID = ProductID; }
+  //   if (ProductName){ Request.ProductName = ProductName; }
+  //   if (Trademark){ Request.Trademark = Trademark; }
+  //   if (ManufactureCountry){ Request.ManufactureCountry = ManufactureCountry; }
+  //   if (PurchasePrice){ Request.PurchasePrice = PurchasePrice; }
+  //   if (WholesalePrice){ Request.WholesalePrice = WholesalePrice; }
+  //   if (RetailPrice){ Request.RetailPrice = RetailPrice; }
+  //   if (ProductQuantity){ Request.ProductQuantity = ProductQuantity; }
+
+  //   Response = axios.get('http://localhost:8000/apis/v1.0/commercial', {params: Request})
+  //     .catch((error) => {console.log(error);});
+  //   console.log(Response.data);
+  //   //setAddedProduct(false);
+  // }, [ProductID, ProductName, Trademark, ManufactureCountry, PurchasePrice, WholesalePrice, RetailPrice, ProductQuantity, AddedProduct]);
+
   return(
     <div className="Tab-content" id="Products-list">
       <div className="Table-container">
         <table className="Table" id="Products-table">
-          <tr>
-            <th>م</th>
-            <th>الرقم التعريفي</th>
-            <th>اسم المنتج</th>
-            <th>الكمية</th>
-            <th>السعر</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>520</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>555</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>م</th>
+              <th>الرقم التعريفي</th>
+              <th>اسم المنتج</th>
+              <th>العلامة التجارية</th>
+              <th>بلد التصنيع</th>
+              <th>سعر الشراء</th>
+              <th>سعر بيع الجملة</th>
+              <th>سعر بيع التجزئة</th>
+              <th>الكمية</th>
+            </tr>
+          </thead>
+          <tbody>
+            <FetchProducts></FetchProducts>
+          </tbody>
         </table>
+        <AddProductForm></AddProductForm>
       </div>
       <div className='Side-bar'>
-        <button className="Sidebar-button" onClick={(event) => AddProduct(event)}>إضافة منتج</button>
-        <button className="Sidebar-button" onClick={(event) => EditProduct(event)}>تعديل منتج</button>
-        <button className="Sidebar-button" onClick={(event) => DeleteProduct(event)}>حذف منتج</button>
+        <button className="Sidebar-button" onClick={(event) => ShowAddProductForm(event)}>إضافة منتج</button>
+        <button className="Sidebar-button" onClick={(event) => ShowEditProductForm(event)}>تعديل منتج</button>
         <button className="Sidebar-button">بحث</button>
         <button className="Sidebar-button">طباعة المنتجات</button>
       </div>
     </div>
   );
-  function LoadProducts(event){
-    axios.get('http://localhost:8000/apis/v1.0/commercial', {params:{RequestType:"GetProductInfo",ProductID:12}},
-    ).then((response) => {console.log(response.data);}).catch((error) => {console.log(error);});
+  function FetchProducts(){
+    try{
+      console.log(Response.Data);
+    return Object.keys(Response.Data).map((key, index) => (
+      <tr>
+        <td>{index + 1}</td>
+        <td>{Response.Data[key].Product_ID}</td>
+        <td>{Response.Data[key].Product_Name}</td>
+        <td>{Response.Data[key].Trademark}</td>
+        <td>{Response.Data[key].Manufacture_Country}</td>
+        <td>{Response.Data[key].Purchase_Price}</td>
+        <td>{Response.Data[key].Wholesale_Price}</td>
+        <td>{Response.Data[key].Retail_Price}</td>
+        <td>{Response.Data[key].Quantity}</td>
+      </tr>
+    ))}
+    catch{return ""}
+
   }
-  function AddProduct(event){
-    console.log("Add Product");
+  function ShowAddProductForm(event){
+    document.getElementById("Add-product-form").style.display = "block";
   }
-  function EditProduct(event){
+  function ShowEditProductForm(event){
     console.log("Edit Product");
   }
-  function DeleteProduct(event){
-    console.log("Delete Product");
+
+
+  function AddProductForm(){
+
+    return(
+      <div className="Form-container" id="Add-product-form">
+        <div className="Form">
+          <div>
+            <button className='Form-close' onClick={(event) => CloseForm(event)}>X</button>
+          </div>
+          <div>
+            <label>اسم المنتج</label>
+            <input type="text" id="Add-product-name"></input>
+          </div>
+          <div>
+            <label>العلامة التجارية</label>
+            <input type="text" id="Add-product-trademark"></input>
+          </div>
+          <div>
+            <label>بلد التصنيع</label>
+            <input type="text" id="Add-product-manufacture-country"></input>
+          </div>
+          <div>
+            <label>سعر الشراء</label>
+            <input type="number" id="Add-product-purchase-price"></input>
+          </div>
+          <div>
+            <label>سعر بيع الجملة</label>
+            <input type="number" id="Add-product-wholesale-price"></input>
+          </div>
+          <div>
+            <label>سعر بيع التجزئة</label>
+            <input type="number" id="Add-product-retail-price"></input>
+          </div>
+          <div>
+            <label>الكمية</label>
+            <input type="number" id="Add-product-partial-quantity-precision"></input>
+          </div>
+          <div>
+            <button onClick={(event) => AddProduct(event)}>إضافة</button>
+          </div>
+        </div>
+      </div>
+    );
+    function AddProduct(event){
+      Request = {RequestType:"AddProduct", StoreID:StoreID,
+        ProductName:document.getElementById("Add-product-name").value,
+        Trademark:document.getElementById("Add-product-trademark").value,
+        ManufactureCountry:document.getElementById("Add-product-manufacture-country").value,
+        PurchasePrice:document.getElementById("Add-product-purchase-price").value,
+        WholesalePrice:document.getElementById("Add-product-wholesale-price").value,
+        RetailPrice:document.getElementById("Add-product-retail-price").value,
+        PartialQuantityPrecision:document.getElementById("Add-product-partial-quantity-precision").value
+      };
+
+      
+      axios.get('http://localhost:8000/apis/v1.0/commercial', {params: Request},)
+        .then()
+        .catch((error) => {console.log(error);});
+      setAddedProduct(true);
+      console.log(AddedProduct);
+    
+    }
+  
+    function CloseForm(event){
+      document.getElementsByClassName("Form-container")[0].style.display = "none";
+    }
   }
 }
+
+
 export default App;
